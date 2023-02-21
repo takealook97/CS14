4b2213fc58290c9f71ffb0be672e2436ab957676
takealook <takealook97@naver.com>
2023-02-21 09:51:33 +0900

    구현완료 but 속도 느림

--- Data.java
+++ Data.java
@@ -3,15 +3,14 @@ import java.util.Random;
 
 public class Data {
     public static void main(String[] args) throws SQLException {
-//        String url = "jdbc:mysql://localhost:3306/mydb";
-//        String userName = "takealook";
-//        String password = "0205";
-//        Connection con = DriverManager.getConnection(url, userName, password);
-//        Statement st = con.createStatement();
-//        ResultSet rs = st.executeQuery("select * from users");
+        String url = "jdbc:mysql://localhost:3306/mydb";
+        String userName = "takealook";
+        String password = "0205";
+        Connection con = DriverManager.getConnection(url, userName, password);
+        PreparedStatement pst = null;
+//        ResultSet rs = null;
 
-
-        String[] words = new String[]{"force", "communicate", "develop", "earth", "protect", "signal", "animal", "rainstorm", "stone", "rabbits",
+        String[] words = {"force", "communicate", "develop", "earth", "protect", "signal", "animal", "rainstorm", "stone", "rabbits",
                 "matter", "hunt", "channel", "goofy", "red", "smoke", "poor", "equal", "calculating", "impress", "collect", "bear", "fail",
                 "gray", "wrench", "diligent", "bikes", "sleepy", "amusement", "theory", "slimy", "unruly", "accidental", "jar", "rub", "quince",
                 "muddle", "fade", "hurried", "limit", "foamy", "silly", "influence", "periodic", "feigned", "sugar", "level", "fog", "flowers",
@@ -19,10 +18,14 @@ public class Data {
                 "unknown", "regular", "bite", "excite", "share", "tart", "bang", "poised", "actually", "screeching", "healthy", "spring", "egg",
                 "ink", "sloppy", "river", "wanting", "scissors", "famous", "flavor", "puncture", "calm", "toes", "curious", "spiffy", "slim",
                 "team", "silk", "ask", "caring", "spiders", "rely", "pancake", "expansion", "dangerous", "taste", "shop", "room", "historical"};
-        int count = 0;
+        int count = 1;
         Random rd = new Random();
 
-        while (count < 100) {
+        while (count <= 1000000) {
+            String nickname;
+            int money;
+            String lastVisit;
+
             StringBuilder randomString = new StringBuilder();
             for (int i = 0; i < 3; i++) {
                 randomString.append((char) (rd.nextInt(26) + 97));//랜덤 문자열 3자리
@@ -32,8 +35,8 @@ public class Data {
                 randomNumber.append(rd.nextInt(10));//랜덤 숫자 4자리
             }
 
-            String nickname = words[rd.nextInt(100)] + randomString + randomNumber;
-            int money = rd.nextInt(100000) + 1;
+            nickname = words[rd.nextInt(100)] + randomString + randomNumber;
+            money = rd.nextInt(100000) + 1;
 
             String[] dateTime = new String[6];//YYYY-MM-DD HH:MM:SS
             dateTime[0] = Integer.toString(2023);
@@ -46,16 +49,24 @@ public class Data {
             dateTime[3] = String.format("%02d", rd.nextInt(24));
             dateTime[4] = String.format("%02d", rd.nextInt(60));
             dateTime[5] = String.format("%02d", rd.nextInt(60));
-            String last_visit = dateTime[0] + "-" + dateTime[1] + "-" + dateTime[2] + " " + dateTime[3] + ":" + dateTime[4] + ":" + dateTime[5];
+            lastVisit = dateTime[0] + "-" + dateTime[1] + "-" + dateTime[2] + " " + dateTime[3] + ":" + dateTime[4] + ":" + dateTime[5];
 
-//            System.out.println(nickname);
-//            System.out.println(money);
-//            System.out.println(last_visit + "\n");
+            String SQL = "INSERT INTO user_log VALUES (?, ?, ?)";
+            pst = con.prepareStatement(SQL);
+            pst.setString(1, nickname);
+            pst.setString(2, String.valueOf(money));
+            pst.setString(3, lastVisit);
+            pst.execute();
+
+            System.out.println(nickname);
+            System.out.println(money);
+            System.out.println(lastVisit);
+            System.out.println("status : " + count + "\n");
+
             count++;
         }
-
 //        rs.close();
-//        st.close();
-//        con.close();
+        pst.close();
+        con.close();
     }
 }
