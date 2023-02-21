20ded24fcd0e3eecd1b68e065e167e16da8af3bd
takealook <takealook97@naver.com>
2023-02-21 12:26:53 +0900

    24시간 이상-> 500~600초로 단축

--- Data.java
+++ Data.java
@@ -6,10 +6,11 @@ public class Data {
         String url = "jdbc:mysql://localhost:3306/mydb";
         String userName = "takealook";
         String password = "0205";
-        Connection con = DriverManager.getConnection(url, userName, password);
+        Connection con = null;
         PreparedStatement pst = null;
-//        ResultSet rs = null;
+        String SQL = "INSERT INTO user_log VALUES (?, ?, ?)";
 
+//        ResultSet rs = null;
         String[] words = {"force", "communicate", "develop", "earth", "protect", "signal", "animal", "rainstorm", "stone", "rabbits",
                 "matter", "hunt", "channel", "goofy", "red", "smoke", "poor", "equal", "calculating", "impress", "collect", "bear", "fail",
                 "gray", "wrench", "diligent", "bikes", "sleepy", "amusement", "theory", "slimy", "unruly", "accidental", "jar", "rub", "quince",
@@ -21,52 +22,87 @@ public class Data {
         int count = 1;
         Random rd = new Random();
 
-        while (count <= 1000000) {
-            String nickname;
-            int money;
-            String lastVisit;
-
-            StringBuilder randomString = new StringBuilder();
-            for (int i = 0; i < 3; i++) {
-                randomString.append((char) (rd.nextInt(26) + 97));//랜덤 문자열 3자리
-            }
-            StringBuilder randomNumber = new StringBuilder();
-            for (int i = 0; i < 4; i++) {
-                randomNumber.append(rd.nextInt(10));//랜덤 숫자 4자리
-            }
-
-            nickname = words[rd.nextInt(100)] + randomString + randomNumber;
-            money = rd.nextInt(100000) + 1;
-
-            String[] dateTime = new String[6];//YYYY-MM-DD HH:MM:SS
-            dateTime[0] = Integer.toString(2023);
-            dateTime[1] = String.format("%02d", rd.nextInt(2) + 1);
-            if (dateTime[1].equals("01")) {//1월일 경우
-                dateTime[2] = Integer.toString(rd.nextInt(11) + 21);//21~31일
-            } else if (dateTime[1].equals("02")) {//2월일 경우
-                dateTime[2] = String.format("%02d", rd.nextInt(21) + 1);//1~21일
-            }
-            dateTime[3] = String.format("%02d", rd.nextInt(24));
-            dateTime[4] = String.format("%02d", rd.nextInt(60));
-            dateTime[5] = String.format("%02d", rd.nextInt(60));
-            lastVisit = dateTime[0] + "-" + dateTime[1] + "-" + dateTime[2] + " " + dateTime[3] + ":" + dateTime[4] + ":" + dateTime[5];
-
-            String SQL = "INSERT INTO user_log VALUES (?, ?, ?)";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, password);
+            con.setAutoCommit(false);
             pst = con.prepareStatement(SQL);
-            pst.setString(1, nickname);
-            pst.setString(2, String.valueOf(money));
-            pst.setString(3, lastVisit);
-            pst.execute();
 
-            System.out.println(nickname);
-            System.out.println(money);
-            System.out.println(lastVisit);
-            System.out.println("status : " + count + "\n");
+            while (count <= 1000000) {
+                String nickname;
+                int money;
+                String lastVisit;
 
-            count++;
-        }
+                StringBuilder randomString = new StringBuilder();
+                for (int i = 0; i < 3; i++) {
+                    randomString.append((char) (rd.nextInt(26) + 97));//랜덤 문자열 3자리
+                }
+                StringBuilder randomNumber = new StringBuilder();
+                for (int i = 0; i < 4; i++) {
+                    randomNumber.append(rd.nextInt(10));//랜덤 숫자 4자리
+                }
+                nickname = words[rd.nextInt(100)] + randomString + randomNumber;
+
+                money = rd.nextInt(100000) + 1;
+
+                String[] dateTime = new String[6];//YYYY-MM-DD HH:MM:SS
+                dateTime[0] = Integer.toString(2023);
+                dateTime[1] = String.format("%02d", rd.nextInt(2) + 1);
+                if (dateTime[1].equals("01")) {//1월일 경우
+                    dateTime[2] = Integer.toString(rd.nextInt(11) + 21);//21~31일
+                } else if (dateTime[1].equals("02")) {//2월일 경우
+                    dateTime[2] = String.format("%02d", rd.nextInt(21) + 1);//1~21일
+                }
+                dateTime[3] = String.format("%02d", rd.nextInt(24));
+                dateTime[4] = String.format("%02d", rd.nextInt(60));
+                dateTime[5] = String.format("%02d", rd.nextInt(60));
+                lastVisit = dateTime[0] + "-" + dateTime[1] + "-" + dateTime[2] + " " + dateTime[3] + ":" + dateTime[4] + ":" + dateTime[5];
+
+
+//            pst.setString(1, nickname), (2, String.valueOf(money)), (3, lastVisit);
+                pst.setString(1, nickname);
+                pst.setString(2, String.valueOf(money));
+                pst.setString(3, lastVisit);
+                pst.addBatch();
+                pst.clearParameters();
+                if (count % 10000 == 0) {
+                    pst.executeBatch();
+                    pst.clearBatch();
+                    con.commit();
+                }
+
+                System.out.println(nickname);
+                System.out.println(money);
+                System.out.println(lastVisit);
+                System.out.println("status : " + count + "\n");
+
+                count++;
+            }
+
+            pst.executeBatch();
+            con.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                con.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            } finally {
+                if (pst != null) try {
+                    pst.close();
+                    pst = null;
+                } catch (SQLException ex) {
+                }
+                if (con != null) try {
+                    con.close();
+                    con = null;
+                } catch (SQLException ex) {
+                }
+            }
 //        rs.close();
-        pst.close();
-        con.close();
+            pst.close();
+            con.close();
+        }
     }
 }
